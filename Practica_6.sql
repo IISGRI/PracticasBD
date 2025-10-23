572cd78cf94e917787350ee995a1427685047594
IISGRI <116135579+IISGRI@users.noreply.github.com>
2025-10-23 17:05:31 -0600

    Update Practica_6.sql

--- Practica_6.sql
+++ Practica_6.sql
@@ -1,1 +1,58 @@
 -- PRACTICA LABORATORIO 6
+
+-- EJERCICIOS DE FUNCIONES DE FECHA
+
+
+-- 1. Muestra la fecha actual.
+
+-- 2. ÀQue fecha sera dentro de una semana?
+
+-- 3. ÀCual es la antiguedad de los clientes? (a–os que han pasado desde que se dieron de alta).
+
+-- 4. Muestra en una columna solo el dia de la fecha de Hoy y en otra el nombre del dia de hoy.
+
+-- 5. Muestra en columnas separadas al a–o, mes, dia, hora y minuto de la fecha de alta.
+
+-- 6. Si el cliente compra algo hoy a mensualidades, ÀCuando seran los primeros 3 pagos?
+
+-- 7. Muestra la antigŸedad de los Clientes en A–os, meses y dias.
+
+-- 8. Muestra " el día de hoy [11/03/2010]  es [Sabado] "
+
+-- 9. Muestra " [Edgar Catalan] se dio de alta el [sabado 11/03/2010] "
+
+-- 10. Muestra la fecha de Alta, sin mostrar la hora.
+
+-- 11. Muestra a los Clientes que se dieron de Alta en el 2010.
+
+-- 12. Muestra a los que dieron de alta en Diciembre de cualquier a–o.
+
+-- 13. Muestra a los que se dieron de Alta en Febrero del 2009.
+
+-- 14. Muestra a los que se dieron de Alta el 15 de Agosto del 2010.
+
+-- 15. Muestra a los clientes que tienen menos de 6 meses que se dieron de alta.
+
+-- 16. Muestra a los clientes que tienen menos de 18 a–os
+
+-- 17. Muestra a los Clientes que hoy cumplen a–os.
+
+-- 18. Muestra a los clientes que cumplen a–os en diciembre
+
+-- 19. Muestra a los clientes que cumplen a–os Enero Febrero, Marzo o Abril
+
+-- 20. Muestra a los Clientes que cumplen a–os este mes.
+
+-- 21. Muestra a los clientes que cumplen a–os el pr—ximo mes
+
+-- 22. Muestra a los clientes que cumplen a–os este o el pr—ximo mes
+
+-- 23. Muestra a los clientes que cumplen a–os esta semana 
+
+-- 24. Muestra a los clientes que cumplen a–os ma–ana
+
+-- 25. Muestra a los clientes que cumplen a–os en cualquiera de los pr—ximos 7 d’as
+
+-- 26. Muestra a los clientes que cumplen a–os en el pr—ximo s‡bado
+
+-- 27. Para los clientes con una deuda de mas de 5000 pesos crea el texto " Estimado cliente [Edgar Catalan] le recordamos que al dia de hoy [11/03/2011] usted nos debe [6000] le ofrecemos la posibilidad de pagar a 6 meses con el 10% de interes, por lo que esperamos su primer pago de [1100] pesos a mas tardar el dia 11 de abril del 2011. Por su atencion Gracias".
